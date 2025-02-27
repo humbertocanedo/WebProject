e2df46efbe4c1b35d708396c37241cbe16e582ce
Humberto Canedo Cebreros <116711621+humbertocanedo@users.noreply.github.com>
2025-02-27 16:24:00 -0600

    login a medias

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+// console.log("Hola desde javascript !!!");
+
+// alert("hola");
+
+// confirm("quieres borrar todo?");
+
+// prompt("dime tu nombre");
+
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+const login = () => {
+    // console.log(username.value, password.value);
+    if (username.value === "humbe" && password.value === "123"){
+        sessionStorage.setItem("name", "Humbe Canedo")
+        window.location = "/pages/home.html";
+    } 
+    else {
+        alert("credenciales incorrectas");
+    }
+};
+
+
+const btnLogin = document.getElementById("btnLogin");
+
+btnLogin.addEventListener("click", login);
+
+password.addEventListener("keydown", (e) => {
+    if (e.key === "'Enter"){
+        login();
+    }
+});
